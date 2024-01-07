69446362092e743b53e60e589c0926781d58c417
bishwaTharu <bishwanaththaru34@gmail.com>
2024-01-07 19:41:45 +0545

    Text splitter fixed

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 from Components.docs_splitter import doc_Split 
+from Components.docs_loader import load_db
 
-with open("data/some_txt.txt") as f:
-    some_txt = f.read()
-print(type(some_txt))
+documents = load_db(path="./data")
+rSplit_result, cSplit_result = doc_Split(chuck_size=300,chuck_overlab=50,documets=documents,seperators=["\n\n\n", "\n", " ", ""])
 
-rSplit_result, cSplit_result = doc_Split(chuck_size=1000,chuck_overlab=150,documets=[some_txt],seperators=["\n\n", "\n", " ", ""])
-print(f'==============R_split======================\n {rSplit_result.split_text(some_txt)}')
-print(f'==============R_split length======================\n {len(rSplit_result.split_text(some_txt))}')
-print(f'==============0_split length======================\n {rSplit_result.split_text(some_txt)[0]}')
-# print(f'===============c_split==================\n {cSplit_result.split_text(some_txt)}')
+print(f'==============R_split length======================\n {len(rSplit_result.split_documents(documents))}')
+print(f'==============0_split item======================\n {rSplit_result.split_documents(documents)[0]}')
