5e8418e999b628fff52de47717a13c02827f846c
bishwaTharu <bishwanaththaru34@gmail.com>
2024-01-07 19:17:09 +0545

    testing Text splitter

--- Components/docs_splitter.py
+++ Components/docs_splitter.py
@@ -4,9 +4,7 @@ from typing import Any
 def doc_Split(chuck_size:int, chuck_overlab:int,seperators:Any,documets:Any):
     r_splitter = RecursiveCharacterTextSplitter(chunk_size=chuck_size,chunk_overlap=chuck_overlab,separators=seperators)
     c_splitter = CharacterTextSplitter(chunk_size=chuck_size,chunk_overlap=chuck_overlab,separator=seperators)
-    r_docs = r_splitter.split_documents(documents=documets)
-    c_docs = c_splitter.split_documents(documents=documets)
-    return r_docs,c_docs
+    return r_splitter,c_splitter
 
 # [Test]
 
--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from Components.docs_splitter import doc_Split
 
 with open("data/some_txt.txt") as f:
     some_txt = f.read()
-print(some_txt)
+print(type(some_txt))
 
-rSplit_result, cSplit_result = doc_Split(chuck_size=100,chuck_overlab=50,documets=[some_txt],seperator=" ")
-print(f'==============R_split======================\n {rSplit_result}')
-print(f'===============c_split==================\n {cSplit_result}')
+rSplit_result, cSplit_result = doc_Split(chuck_size=1000,chuck_overlab=150,documets=[some_txt],seperators=["\n\n", "\n", " ", ""])
+print(f'==============R_split======================\n {rSplit_result.split_text(some_txt)}')
+print(f'==============R_split length======================\n {len(rSplit_result.split_text(some_txt))}')
+print(f'==============0_split length======================\n {rSplit_result.split_text(some_txt)[0]}')
+# print(f'===============c_split==================\n {cSplit_result.split_text(some_txt)}')
